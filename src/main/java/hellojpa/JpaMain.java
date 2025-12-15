1b1fcf61c3740f6e8025727a77c5ccd357eab071
Yoonho Cha <0119hoya@naver.com>
2025-12-15 22:49:24 +0900

    section 10: 값 타입과 불변 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "1000"));
-            member.setWorkPeriod(new Period());
+            // 값을 복사해서 사용해야함 ! 공유하는 것은 위험 -> 생성자로만 값 접근해야함
+            // ** 불변이라는 작은 제약으로 부작용이라는 큰 재앙을 막을수 있다. **
+            Address address = new Address("city", "street", "1000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
 
+            // 수정하려면 이렇케 통으로 바꿔야함
+            Address newAddress = new Address("New City", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
